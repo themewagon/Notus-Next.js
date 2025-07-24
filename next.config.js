1e1b572e364887571fcdc5b2cccca7e06b5b6b78
KhanAnupamShafi <deep71.bd@gmail.com>
2025-07-24 15:09:17 +0600

    added next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 // next.config.js
+const repo = "Notus-Next.js"; // replace with your actual repo name
+
 module.exports = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: { unoptimized: true, disableStaticImages: true },
 };
